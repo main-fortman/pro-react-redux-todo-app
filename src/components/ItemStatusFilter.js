7365d799d5297d8f658f1888f128a4b39d4def7e
Oleg Baranov <oleg.baranov@skywindgroup.com>
2021-06-25 17:39:36 +0300

    feat: lesson 32: add status and styles

---
+++ src/components/ItemStatusFilter.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './ItemStatusFilter.css';
+
+
+export default function ItemStatusFilter() {
+  return (
+    <div className="btn-group">
+      <button 
+        type="button"
+        className="btn btn-info">
+            All
+      </button>
+      <button 
+        type="button"
+        className="btn btn-outline-secondary">
+            Active
+      </button>
+      <button 
+        type="button"
+        className="btn btn-outline-secondary">
+        Done
+      </button>
+    </div>
+  );
+};
