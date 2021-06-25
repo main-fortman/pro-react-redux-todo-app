f4fa4142b00fd5bfa32c368fc7a2c61a9985f1cf
Oleg Baranov <oleg.baranov@skywindgroup.com>
2021-06-25 14:52:14 +0300

    feat: add simple interface for todo list

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log("Hello!");
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const el = (
+  <div>
+    <h1>My Todo List</h1>
+    <input placeholder='search'/>
+    <ul>
+      <li>Learn React</li>
+      <li>Build App</li>
+    </ul>
+  </div>
+);
+
+ReactDOM.render(el, document.getElementById('root'));
