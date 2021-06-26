6181d489073b8ca5165c736bd9e5e5564d326cc9
Oleg Baranov <oleg.baranov@skywindgroup.com>
2021-06-26 17:23:46 +0300

    refactor: lesson 34: change 2 func. components to class

--- src/components/itemStatusFilter/ItemStatusFilter.js
+++ src/components/itemStatusFilter/ItemStatusFilter.js
@@ -2,24 +2,29 @@ import React from 'react';
 import './ItemStatusFilter.css';
 
 
-export default function ItemStatusFilter() {
-  return (
-    <div className="btn-group">
-      <button 
-        type="button"
-        className="btn btn-info">
-            All
-      </button>
-      <button 
-        type="button"
-        className="btn btn-outline-secondary">
-            Active
-      </button>
-      <button 
-        type="button"
-        className="btn btn-outline-secondary">
-        Done
-      </button>
-    </div>
-  );
+export default class ItemStatusFilter extends React.Component {
+
+  render() {
+    return (
+      <div className="btn-group">
+        <button 
+          type="button"
+          className="btn btn-info">
+              All
+        </button>
+        <button 
+          type="button"
+          className="btn btn-outline-secondary">
+              Active
+        </button>
+        <button 
+          type="button"
+          className="btn btn-outline-secondary">
+          Done
+        </button>
+      </div>
+    );
+
+  }
+
 };
