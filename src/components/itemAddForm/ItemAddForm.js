1942e658d9ebf106790da5412880c215b90445ae
Oleg Baranov <oleg.baranov@skywindgroup.com>
2021-06-28 14:27:38 +0300

    feat: lessons 44-45: add new todo item

--- src/components/itemAddForm/ItemAddForm.js
+++ src/components/itemAddForm/ItemAddForm.js
@@ -3,15 +3,41 @@ import './ItemAddForm.css';
 
 
 export default class ItemAddForm extends React.Component {
+
+    state = {
+        text: ''
+    }
+
+    onLabelChange = (e) => {
+        this.setState({text: e.target.value});
+    }
+
+    onSubmit = (e) => {
+        e.preventDefault();
+        if (this.state.text) {
+            this.props.onClickAdd(this.state.text);
+            this.setState({text: ''});
+            // e.target.reset();
+        }
+    }
+
     render() {
         return (
-            <div className='item-add-form'>
-                <button 
-                    className='btn btn-outline-secondary'
-                    onClick={() => this.props.onClickAdd('random')}>
+            <form 
+                className='item-add-form d-flex'
+                onSubmit={this.onSubmit}
+            >
+                <input 
+                    type='text' 
+                    placeholder='What needs to be done?'    
+                    className='form-control' 
+                    value={this.state.text}
+                    onChange={this.onLabelChange}
+                />
+                <button disabled={!this.state.text} className='btn btn-outline-secondary'>
                     Add Item
                 </button>
-            </div>
+            </form>
         )
     }
 }
